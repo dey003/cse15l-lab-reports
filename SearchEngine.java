3afa348de929a16cf38a02c8ada58561684aa059
dey003 <146765754+dey003@users.noreply.github.com>
2023-10-10 15:47:14 -0700

    Updated SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -5,7 +5,7 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     String searches[];
-    int counter = 0;
+    int numSearches = 0;
 
     public static String[] addString(int n, String arr[], String str) {
         String newarr[] = new String[n + 1];
@@ -19,11 +19,58 @@ class Handler implements URLHandler {
         return newarr;
     }
 
+    public static String findStrings(String arr[], String str) {
+        String matches = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].contains(str)) {
+                if (matches.equals(""))
+                    matches = arr[i];
+                else
+                    matches = matches + ", " + arr[i];
+            }
+        }
+        return matches;
+    }
+
     public String handleRequest(URI url) {
         if (url.getPath().equals("/add")) {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
-                searches = addString(counter, searches, parameters[1]);
-                counter++;
+                searches = addString(numSearches, searches, parameters[1]);
+                numSearches++;
+                return String.format("%s added to searchable strings! The number of strings is now %d!", parameters[1], numSearches);
             }
-        } 
+        } else if (url.getPath().equals("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                if (numSearches > 0)
+                    return findStrings(searches, parameters[1]);
+                else
+                    return "Search not found!";
+            }
+        } else if (url.getPath().equals("/")) {
+            String strings = "";
+            for (int i = 0; i < numSearches; i++) {
+                if (strings.equals(""))
+                    strings = searches[i];
+                else
+                    strings = strings + ", " + searches[i];
+            }
+            return String.format("Current list of searchable strings: %s", strings);
+        }
+        return "404 Not Found!";
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
